47552ce473bd39bc56e9af4261f29cf7a8dae50f
scarlet-project <mauricioazevedogou@gmail.com>
2023-03-15 20:02:31 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from flask import Flask,render_template
 
+class Jogos():
+    def __init__(self, nome, tipo, console):
+        self.nome = nome
+        self.tipo = tipo
+        self.console = console
+
+
 app = Flask(__name__)
 
 
 @app.route('/')
-def ola():
-    lista = ['Skyrim','Hogwarts Legacy','The Witcher 3']
+def Flask():
+    jogo1 = Jogos('Skyrim', 'Rpg','Multiplataforma')
+    jogo2 = Jogos('Pokemon X','Rpg','Nintendo 3DS')
+    lista = [jogo1,jogo2]
     return render_template('index.html', titulo = 'Mauricio', jogos = lista)
 
 app.run()
