af452cac661a6aaffaa607c262984af17fbe514c
scarlet-project <mauricioazevedogou@gmail.com>
2023-03-15 16:10:59 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def ola():
-    return '<h1>Olá SrCatra</h1>'
-
+    return render_template('index.html')
 
 app.run()
 
